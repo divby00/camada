a440b10797616478509354bee937b5f9e9a5953f
divby00 <divisionby0x00@gmail.com>
2019-11-03 09:32:22 +0100

    More work on PDF generation (WIP)

--- src/main/java/org/wildcat/camada/utils/PdfUtils.java
+++ src/main/java/org/wildcat/camada/utils/PdfUtils.java
@@ -1,19 +1,26 @@
 package org.wildcat.camada.utils;
 
 import javafx.collections.ObservableList;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.layout.SharedContext;
+import org.xhtmlrenderer.pdf.ITextOutputDevice;
 import org.xhtmlrenderer.pdf.ITextRenderer;
+import org.xhtmlrenderer.pdf.ITextUserAgent;
 
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URL;
 import java.time.LocalDateTime;
 
+@Slf4j
 public class PdfUtils {
 
     public static void main(String... args) {
-        export(null, "data.pdf");
+        export(null, "date.pdf");
     }
 
     public static <T> Boolean export(ObservableList<T> items, String fileName) {
@@ -21,7 +28,7 @@ public class PdfUtils {
         try {
             ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
 
-            templateResolver.setPrefix("/templates/");
+            templateResolver.setPrefix("/pdf/templates/");
             templateResolver.setSuffix(".html");
             templateResolver.setTemplateMode("HTML");
 
@@ -34,7 +41,9 @@ public class PdfUtils {
 
             String html = templateEngine.process("index", context);
             OutputStream outputStream = new FileOutputStream(fileName);
-            ITextRenderer renderer = new ITextRenderer();
+            ITextRenderer renderer = new ITextRenderer(300, 40);
+            renderer.getSharedContext().setUserAgentCallback(new UserAgentCallback(renderer.getOutputDevice(), renderer.getSharedContext()));
+            FileUtils.write(new File("out.html"), html);
             renderer.setDocumentFromString(html);
             renderer.layout();
             renderer.createPDF(outputStream);
@@ -45,4 +54,63 @@ public class PdfUtils {
         }
         return result;
     }
+
+
+    private static class UserAgentCallback extends ITextUserAgent {
+        public UserAgentCallback(ITextOutputDevice outputDevice, SharedContext sharedContext) {
+            super(outputDevice);
+            setSharedContext(sharedContext);
+        }
+
+        @Override
+        public String resolveURI(String uri) {
+            return uri;
+        }
+
+        @Override
+        protected InputStream resolveAndOpenStream(String uri) {
+            java.io.InputStream is = null;
+            URL url = null;
+            try {
+                url = new ClassPathResource("/pdf/" + uri).getURL();
+            } catch (IOException e) {
+                log.error("bad URL given: " + uri, e);
+            }
+            if (url == null) {
+                log.error("Didn't find resource [" + uri + "].");
+                return null;
+            }
+            try {
+                is = url.openStream();
+            } catch (java.net.MalformedURLException e) {
+                log.error("bad URL given: " + uri, e);
+            } catch (java.io.FileNotFoundException e) {
+                log.error("item at URI " + uri + " not found");
+            } catch (java.io.IOException e) {
+                log.error("IO problem for " + uri, e);
+            }
+            return is;
+        }
+    }
+
+    /*
+    class CustomTextUserAgent extends ITextUserAgent {
+        public CustomTextUserAgent(ITextOutputDevice outputDevice) {
+            super(outputDevice);
+        }
+
+        @Override
+        protected InputStream resolveAndOpenStream(String uri) {
+            if (uri.startsWith("file:")) {
+                String path = uri.substring("file:".length());
+
+                InputStream is = getClass().getClassLoader().getResourceAsStream(String.format("reports%s", path));
+                if (is != null) {
+                    return is;
+                }
+            }
+            return super.resolveAndOpenStream(uri);
+        }
+    }
+     */
 }
